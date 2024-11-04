f1b1dce6aad494164439297c7ffb6a57213df0fe
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-11-05 00:37:17 +0200

    Initial setup express

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,16 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next)=> {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+});
+
+
+
 
 app.listen(8080, "Server started on port 8080!");
